1422c413885d8ab443af4af5fbc79dcfde7787a9
ahmed10mohsen74 <106989484+ahmed10mohsen74@users.noreply.github.com>
2022-06-22 17:07:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//document.querySelector("h1").style.color="blue";
+////window.alert("haloo frind");
+//document.write("<h1>hallo <span>frind</span</h1>");
+//console.log("hallo console.js");
+//console.error("error");
+//console.table(["ahmed","mohsen","kamel"]);
+//console.log(typeof true);
+const user = "ahmed-mohsen";
+console.log(user);
+let markUp=`
+<div class="card">
+    <div class="child">
+        <h1>hallo world</h1>
+        <p>paragraph</p>
+    </div>
+</div>
+`;
+`
